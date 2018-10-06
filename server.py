69504c1264669d26cfcf4bc90867f8f482dea516
Kami Karras <ekarras@pnca.edu>
2018-10-06 04:18:40 +0000

    created second route to receive name from homepage form and query different ingredients by the letters.

--- server.py
+++ server.py
@@ -6,11 +6,26 @@ app = Flask(__name__)
 
 app.secret_key = "ABC"
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def show_home():
     """shows the hompage"""
 
     return render_template("homepage.html")
+
+@app.route('/', methods=["POST"])
+def show_drink():
+    """displays the drink"""
+    name = request.form.get('name')
+    ingredients = []
+    for letter in name:
+        if letter.isalpha():
+            letter = letter.upper()
+            mix = Mix.query.filter(Mix.letter==letter).first()
+            ingredients.append(mix.ingredient)
+    ingredients = set(ingredients)
+
+    return render_template("homepage.html",
+                            ingredients=ingredients)
 
 
 if __name__ == "__main__":
